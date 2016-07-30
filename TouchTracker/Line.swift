4740cca2ff4049c27ff4c97da79d1d94c8493bb6
Ash <ash@ashryanbeats.com>
2016-07-30 17:07:34 -0400

    Chapters 17 and 18 complete.

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,17 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Ash Ryan Arnwine on 7/30/16.
+//  Copyright © 2016 Ash Ryan Beats. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+    
+}
